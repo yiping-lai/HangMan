1af2c13d1ef5508921f4838d34a94e0d95ec9052
YiPing <monoyp0211@gmail.com>
2020-07-19 00:50:17 +0000

    changed to ajax

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express=require("express"),
 	app=express(),
 	bodyParser=require("body-parser"),
-	axios = require('axios'),
+	port=process.env.PORT || 3000,
 	dictApi = "https://random-word-api.herokuapp.com/word?number=1";
 
 
@@ -18,27 +18,14 @@ app.use(express.static(__dirname+"/public"));
 // Routes
 //=========================
 app.get("/",function(req,res){
-	// get a random word from dictApi to start the game
-	axios.get(dictApi)
-	.then(function(response){
-		var word=response.data[0];
-		res.render("game",{word:word});
-	}).catch(function(){
-		console.log("ERROR!!!");
-	});
+	// render home page 
+	res.render('game');
 });
 
 
-if (process.env.DEVELOPER==='1'){
-	app.listen(3000, function() { 
-		console.log('Server listening on port 3000'); 
-	});
-}else{
-	app.listen(process.env.PORT, process.env.IP, function(){
-  		console.log('Server listening on port',process.env.PORT); 
-	});
-};
-
+app.listen(port, process.env.IP, function(){
+	console.log('Server listening on port',port); 
+});
 
 
 
--- public/js/game.js
+++ public/js/game.js
@@ -13,16 +13,21 @@ var isOver=false,
 		"/img/Hangman-5.png",			
 		"/img/Hangman-6.png"
 	],
-	totalPoint=images.length-1;
+	totalPoint,
+	answer,
+	dictApi = "https://random-word-api.herokuapp.com/word?number=1";
 
-init();
 
-// initialize all variables and event listeners for input key
+$(document).ready(init);
+
+
+/*--------function definitions------*/
+
 function init(){
-	isOver=false;
-	hiddenLetters=$('.letter').length;
-	$("#guessLeft").text(totalPoint);	
+
+	reset();
 	
+	// event listeners for keyboard
 	for (var i=0;i<inputKeys.length;i++){
 		inputKeys[i].addEventListener("click",function(){	
 			if(!isOver && !this.classList.contains("disabled")){
@@ -31,22 +36,58 @@ function init(){
 			}			
 		})
 	}
+	
+	// evenet listener for newGame
+	$("#newGame").on('click',function(){
+		reset();
+	})
 }
+
+
+// get a new word and reset page layout
+function reset(){
+	$.getJSON(dictApi)
+	.then(function(data){
+		// reset variables	
+		answer=data[0].split('');
+		isOver=false;
+		hiddenLetters=answer.length;
+		totalPoint=images.length-1;
+		$("#guessLeft").text(totalPoint);	
+		$("#endMsg").text('');
+
+		// reset letter box
+		$(".letterBox").empty();
+		for (var i=0;i<answer.length;i++){
+			var newletter=$("<span class='letter'>"+answer[i]+"</span>");
+			$(".letterBox").append(newletter);
+		}
+
+		// enable all inputkeys
+		for (var i=0;i<inputKeys.length;i++){
+			$(inputKeys[i]).removeClass("disabled");
+		}
+	})
+	.catch(function(err){
+		console.log(err);
+	})
+}
+
 
 // control logic when user clicks an input key
 function letterSelect(text){
 	if(isOver){
 		return;
 	}
-	
-	var elements=$(".letter:contains('"+text+"')");		
-	if (elements.length==0){
+		
+	if (!answer.includes(text)){
 		// lose one point if no letter matched
 		deductPoint();
 		$("#guessLeft").text(totalPoint);	
 		
 	}else{
 		// make letter guessed visible and update hidden letter count
+		var elements=$(".letter:contains('"+text+"')");	
 		elements.css('color','black');
 		hiddenLetters-=elements.length;
 		if (hiddenLetters==0){
@@ -69,7 +110,8 @@ function deductPoint(){
 function endGame(){
 	isOver=true;
 	if (totalPoint==0){
-		$("#endMsg").text(" You lose!");			
+		$("#endMsg").text(" You lose!");	
+		// show hidden letters in red
 		var letters=document.querySelectorAll(".letter");
 		for (var i=0; i<letters.length; i++){
 			if (letters[i].style.color==''){
